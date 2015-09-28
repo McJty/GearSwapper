d304d110dfe23fd42b9061d27391f71ee68d62f5
McJty <mcjty1@gmail.com>
2015-09-28 16:16:24 +0200

    Not working yet.

--- src/main/java/mcjty/gearswap/GearSwap.java
+++ src/main/java/mcjty/gearswap/GearSwap.java
@@ -1,4 +1,4 @@
-package romelo333.notenoughwands;
+package mcjty.gearswap;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -15,25 +15,25 @@ import romelo333.notenoughwands.proxy.CommonProxy;
 
 import java.io.File;
 
-@Mod(modid = NotEnoughWands.MODID, name="Not Enough Wands", dependencies =
-        "required-after:Forge@["+ NotEnoughWands.MIN_FORGE_VER+",)",
-        version = NotEnoughWands.VERSION)
-public class NotEnoughWands {
+@Mod(modid = GearSwap.MODID, name="Gear Swapper", dependencies =
+        "required-after:Forge@["+ GearSwap.MIN_FORGE_VER+",)",
+        version = GearSwap.VERSION)
+public class GearSwap {
     public static final String MODID = "NotEnoughWands";
     public static final String VERSION = "1.1.2";
     public static final String MIN_FORGE_VER = "10.13.2.1291";
 
-    @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
-    public static CommonProxy proxy;
+//    @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
+//    public static CommonProxy proxy;
 
-    @Mod.Instance("NotEnoughWands")
-    public static NotEnoughWands instance;
+    @Mod.Instance("gearswap")
+    public static GearSwap instance;
     public static Logger logger;
     public static File mainConfigDir;
     public static File modConfigDir;
     public static Configuration config;
 
-    public static CreativeTabs tabNew = new CreativeTabs("NotEnoughWands") {
+    public static CreativeTabs tabNew = new CreativeTabs("GearSw") {
         @Override
         @SideOnly(Side.CLIENT)
         public Item getTabIconItem() {
