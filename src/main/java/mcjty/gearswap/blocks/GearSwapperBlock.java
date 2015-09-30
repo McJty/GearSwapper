c00478e27e413888e9271879cf9400e84c46e601
McJty <mcjty1@gmail.com>
2015-09-30 11:21:51 +0200

    Fixed the gear swapper TESR

--- src/main/java/mcjty/gearswap/blocks/GearSwapperBlock.java
+++ src/main/java/mcjty/gearswap/blocks/GearSwapperBlock.java
@@ -14,11 +14,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import java.util.List;
+
+import static net.minecraftforge.common.util.ForgeDirection.DOWN;
 
 public class GearSwapperBlock extends Block implements ITileEntityProvider {
     private IIcon iconFront;
@@ -60,7 +63,7 @@ public class GearSwapperBlock extends Block implements ITileEntityProvider {
 
     @Override
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entityLivingBase, ItemStack itemStack) {
-        ForgeDirection dir = getOrientation(world, x, y, z);
+        ForgeDirection dir = determineOrientation(x, y, z, entityLivingBase);
         int meta = world.getBlockMetadata(x, y, z);
         world.setBlockMetadataWithNotify(x, y, z, setOrientation(meta, dir), 2);
 //        restoreBlockFromNBT(world, x, y, z, itemStack);
@@ -90,6 +93,23 @@ public class GearSwapperBlock extends Block implements ITileEntityProvider {
         return getOrientation(meta);
     }
 
+    public static ForgeDirection determineOrientation(int x, int y, int z, EntityLivingBase entityLivingBase) {
+        if (MathHelper.abs((float) entityLivingBase.posX - x) < 2.0F && MathHelper.abs((float)entityLivingBase.posZ - z) < 2.0F) {
+            double d0 = entityLivingBase.posY + 1.82D - entityLivingBase.yOffset;
+
+            if (d0 - y > 2.0D) {
+                return ForgeDirection.UP;
+            }
+
+            if (y - d0 > 0.0D) {
+                return DOWN;
+            }
+        }
+        int l = MathHelper.floor_double((entityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        return l == 0 ? ForgeDirection.NORTH : (l == 1 ? ForgeDirection.EAST : (l == 2 ? ForgeDirection.SOUTH : (l == 3 ? ForgeDirection.WEST : DOWN)));
+    }
+
+
     private static ForgeDirection getOrientation(int meta) {
         return ForgeDirection.getOrientation(meta & 0x7);
     }
@@ -97,4 +117,30 @@ public class GearSwapperBlock extends Block implements ITileEntityProvider {
     private static int setOrientation(int metadata, ForgeDirection orientation) {
         return (metadata & ~0x7) | orientation.ordinal();
     }
+
+    /**
+     * The type of render function that is called for this block
+     */
+//    @Override
+//    public int getRenderType() {
+//        return -1;
+//    }
+
+    /**
+     * If this block doesn't render as an ordinary block it will return False (examples: signs, buttons, stairs, etc)
+     */
+    @Override
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    /**
+     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+     */
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
 }
