544d76bd7d1b4ba124f8c6f995cfdb04b7a456f5
McJty <mcjty1@gmail.com>
2015-11-16 20:49:54 +0100

    Version 1.2.3

--- src/main/java/mcjty/gearswap/GearSwap.java
+++ src/main/java/mcjty/gearswap/GearSwap.java
@@ -22,7 +22,7 @@ import java.io.File;
         version = GearSwap.VERSION)
 public class GearSwap {
     public static final String MODID = "gearswap";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     public static final String MIN_FORGE_VER = "10.13.2.1291";
 
     @SidedProxy(clientSide="mcjty.gearswap.proxy.ClientProxy", serverSide="mcjty.gearswap.proxy.ServerProxy")
